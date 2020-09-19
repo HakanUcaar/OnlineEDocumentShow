d830236f37e7314f6fba4d5151a8d38aee3185d7
Hakan Uçar <hakanucaar@yandex.com>
2020-09-19 13:38:58 +0300

    Updates

---
+++ precache-manifest.960dc365ff662617d284d1fcac215219.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72aeeb3d36933e1047967f661ef550c4",
+    "url": "/OnlineEDocumentShow/index.html"
+  },
+  {
+    "revision": "e9659abcef6232dea0cc",
+    "url": "/OnlineEDocumentShow/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4618421e95ff62d3409a",
+    "url": "/OnlineEDocumentShow/static/js/2.8b52adce.chunk.js"
+  },
+  {
+    "revision": "d466ab9648f013dea081956fc7c904f3",
+    "url": "/OnlineEDocumentShow/static/js/2.8b52adce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9659abcef6232dea0cc",
+    "url": "/OnlineEDocumentShow/static/js/main.1bfdf40f.chunk.js"
+  },
+  {
+    "revision": "1c1222ae0b5be88be072",
+    "url": "/OnlineEDocumentShow/static/js/runtime-main.9da45986.js"
+  }
+]);
